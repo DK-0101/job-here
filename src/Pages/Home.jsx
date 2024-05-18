853fe7d1b35f2fadb0ea5dd8582e689945c54b6d
DK-01-01 <dayksontavares23@gmail.com>
2024-05-18 15:22:55 -0300

    Sidebar done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,17 +2,23 @@ import { useEffect, useState } from "react"
 import Banner from "../components/Banner"
 import Card from "../components/Card";
 import Jobs from "./Jobs";
+import Sidebar from "../sidebar/Sidebar";
 
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
   useEffect(() => {
+    setIsLoading(true)
     fetch("jobs.json").then(res => res.json()).then(data => {
      // console.log(data)
-      setJobs(data)
+      setJobs(data);
+      setIsLoading(false)
     })
-  }, [])
+  },[])
 
   const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
@@ -22,12 +28,30 @@ const Home = () => {
   const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   console.log(filteredItems)
 
-  const handlerChange = (event) => {
+  const handleChange = (event) => {
     setSelectedCategory(event.target.value)
   }
 
-  const handlerClick = (event) => {
+  const handleClick = (event) => {
     setSelectedCategory(event.target.value)
+  }
+
+  const calculatePageRange = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return {startIndex, endIndex};
+  }
+
+  const nextPage = () => {
+    if(currentPage < Math.ceil(filteredItems.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  const prevPage = () => {
+    if(currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
   }
 
   const filteredData = (jobs, selected, query) => {
@@ -36,14 +60,17 @@ const Home = () => {
       filteredJobs = filteredItems;
     }
     if(selected) {
-      filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => (
+      filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => 
         jobLocation.toLowerCase() === selected.toLowerCase() ||
         parseInt(maxPrice) <= parseInt(selected) ||
         salaryType.toLowerCase() === selected.toLowerCase() ||
+        experienceLevel.toLowerCase() === selected.toLowerCase() ||
         employmentType.toLowerCase() === selected.toLowerCase()
-      ));
+      );
       console.log(filteredJobs);
     }
+    const {startIndex, endIndex} = calculatePageRange();
+    filteredJobs = filteredJobs.slice(startIndex, endIndex)
     return filteredJobs.map((data, i) => <Card key={i} data={data}/>)
   }
 
@@ -54,8 +81,28 @@ const Home = () => {
       <Banner query={query} handleInputChange={handleInputChange}/>
 
       <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
-        <div className="bg-white p-4 rounded">Left</div>
-        <div className="col-span-2 bg-white p-4 rounded-sm"><Jobs result={result}/></div>
+        <div className="bg-white p-4 rounded">
+          <Sidebar handleChange={handleChange} handleClick={handleClick} />
+        </div>
+        <div className="col-span-2 bg-white p-4 rounded-sm">
+          {
+            isLoading ? (<p className="font-medium">Loading....</p>) : result.length > 0 ? (<Jobs result={result}/>) : <>
+             <h3 className="text-lg font-bold mb-2">{result.length} Jobs</h3>
+             <p>No data found</p>
+            </>
+          }
+
+          {
+            result.length > 0 ? (
+              <div className="flex justify-center mt-4 space-x-8">
+                <button onClick={prevPage} disabled={currentPage === 1} className="hover:underline">Previous</button>
+                <span className="mx-2">Page {currentPage} of {Math.ceil(filteredItems.length /itemsPerPage)}</span>
+                <button onClick={nextPage} disabled={currentPage === Math.ceil(filteredItems.length / itemsPerPage)} className="hover:underline">Next</button>
+              </div>
+            ) : ""
+          }
+
+        </div>
         <div className="bg-white p-4 rounded">Right</div>
       </div>
     </div>
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Location from './Location';
+import Salary from './Salary';
+import JobPostingData from './JobPostingData';
+import WorkExperience from './WorkExperience';
+import EmploymentType from './EmploymentType';
 
-const Sidebar = () => {
+const Sidebar = ({ handleChange, handleClick }) => {
   return (
-    <div>
-      Sidebar
+    <div className='space-y-5'>
+        <h3 className="text-lg font-bold mb-2">Filters</h3>
+        <Location handleChange={handleChange}/>
+        <Salary handleChange={handleChange} handleClick={handleClick}/>
+        <JobPostingData handleChange={handleChange} />
+        <WorkExperience handleChange={handleChange} />
+        <EmploymentType handleChange={handleChange} />
     </div>
   )
 }
